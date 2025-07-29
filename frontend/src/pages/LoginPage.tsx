3845c63163752fd5cbbe673d09598b02210df7bc
Kavishka-Prabashara <kavishkaprabashara@gmail.com>
2025-07-30 01:01:56 +0530

    Update dashboard Navigation

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -20,7 +20,7 @@ export default function LoginPage() {
     useEffect(() => {
         if (user) {
             alert('Login successful!');
-            navigate('/dashboard');
+            navigate('/dashboard/tasks');
         }
     }, [user, navigate]);
 
